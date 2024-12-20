bd1c2e453fbbe479ac5bf5027bb40f67e9fe25b9
isaac <sangodareisaac92@gmail.com>
2024-12-20 04:16:30 +0100

    redirect if user is Logged in

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,11 +1,26 @@
 "use client";
 
+import { useIsLoggedIn } from "@/lib/helpers";
+import { RootState } from "@/redux/store";
 import LoginPage from "@/subpages/auth/LoginPage";
+import { useRouter } from "next/navigation";
 import React from "react";
+import { useSelector } from "react-redux";
 
 type Props = {};
 
 const Login = (props: Props) => {
+  const router = useRouter();
+  const { isLoggedIn } = useIsLoggedIn({ message: "" });
+  const state = useSelector((state: RootState) => state.user);
+  if (
+    isLoggedIn ||
+    state.user !== null ||
+    state.access_token !== null ||
+    state.token !== null
+  ) {
+    router.push("/dashboard");
+  }
   return <LoginPage />;
 };
 
