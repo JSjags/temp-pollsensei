cf48f567146bb6d1d1e4fb0d6dc09bd8235dba94
isaac <sangodareisaac92@gmail.com>
2025-01-07 15:01:50 +0100

    user registry table update

--- src/subpages/superadmin/UserRegistry.tsx
+++ src/subpages/superadmin/UserRegistry.tsx
@@ -1,15 +1,21 @@
 "use client";
 
-import { Filter } from "lucide-react";
 import { ImFilter } from "react-icons/im";
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { useUserRegistryQuery } from "@/services/superadmin.service";
 import PageControl from "@/components/common/PageControl";
 import DropdownFilter from "@/components/superAdmin/DropdownFilter";
 import { generateInitials } from "@/lib/utils";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { FadeLoader } from "react-spinners";
+import { formatDateOption } from "@/lib/helpers";
 
+interface PathParamsProps {
+  subscription_type?: string;
+  account_type?: string;
+  location?: string;
+  email?: string;
+}
 
 const options = {
   subscription: [
@@ -35,15 +41,39 @@ const options = {
 
 const UserRegistry: React.FC = () => {
   const [currentPage, setCurrentPage] = useState(1);
-  const { data, isLoading, error, refetch } = useUserRegistryQuery({
-    pagesNumber: currentPage,
-  });
+  const [email, setEmail] = useState("");
+  const [selectedFilters, setSelectedFilters] = useState<PathParamsProps>({});
+
+  const path_params: PathParamsProps = {};
+  if (email) path_params.email = email;
+
+  const queryArgs = useMemo(() => {
+    return {
+      pagesNumber: currentPage,
+      path_params: {
+        ...selectedFilters,
+        email: email || undefined,
+      },
+    };
+  }, [currentPage, selectedFilters, email]);
+
+  console.log(selectedFilters)
+  const { data, isLoading, error, refetch } = useUserRegistryQuery(queryArgs);
 
   const totalItems = data?.data?.total || 0;
   const totalPages = Math.ceil(totalItems / 20);
 
   const handleFilterApply = (filterType: string, selectedOptions: string[]) => {
-    console.log(`Applied ${filterType} Filter:`, selectedOptions);
+    setSelectedFilters((prev) => ({
+      ...prev,
+      [filterType]: selectedOptions.length > 0 ? selectedOptions : undefined,
+    }));
+  };
+
+  const resetFilters = () => {
+    setSelectedFilters({});
+    setEmail("");
+    setCurrentPage(1);
   };
 
   const navigatePage = (direction: "next" | "prev") => {
@@ -59,7 +89,16 @@ const UserRegistry: React.FC = () => {
   console.log(data);
   console.log(currentPage);
 
-  const statusColorMap = ["#FFC107", "#3498DB", "#27AE60", "#2980B9", "#2ECC71", "#E74C3C", "#FF5733", "#FF5733", "#FF5733",
+  const statusColorMap = [
+    "#FFC107",
+    "#3498DB",
+    "#27AE60",
+    "#2980B9",
+    "#2ECC71",
+    "#E74C3C",
+    "#FF5733",
+    "#FF5733",
+    "#FF5733",
   ];
   const getRandomColor = () => {
     return statusColorMap[Math.floor(Math.random() * statusColorMap.length)];
@@ -72,7 +111,6 @@ const UserRegistry: React.FC = () => {
         <div className="flex items-center gap-">
           <button className="py-2 px-4 bg-white border border-gray-300 rounded-l-full flex items-center text-sm">
             <ImFilter size={20} />
-            {/* <Filter size={20} /> */}
           </button>
           <button className="py-2 px-4 bg-white border border-gray-300 rounded- flex items-center text-sm">
             <span className="mr-2">Filter By</span>
@@ -93,26 +131,33 @@ const UserRegistry: React.FC = () => {
           </button>
           <DropdownFilter
             title="Subscription"
-            options={options.subscription}
-            onApply={(selected) => handleFilterApply("Subscription", selected)}
+            options={options?.subscription}
+            onApply={(selected) =>
+              handleFilterApply("subscription_type", selected)
+            }
           />
           <DropdownFilter
             title="Account Type"
-            options={options.accountType}
-            onApply={(selected) => handleFilterApply("Account Type", selected)}
+            options={options?.accountType}
+            onApply={(selected) => handleFilterApply("account_type", selected)}
           />
           <DropdownFilter
             title="Location"
-            options={options.location}
+            options={options?.location}
             multiSelect
-            onApply={(selected) => handleFilterApply("Location", selected)}
+            onApply={(selected) => handleFilterApply("location", selected)}
           />
-          <button className="text-red-500 bg-white border-gray-300 py-2 px-4 text-sm border rounded-r-full">
+          <button
+            onClick={resetFilters}
+            className="text-red-500 bg-white border-gray-300 py-2 px-4 text-sm border rounded-r-full"
+          >
             Reset Filter
           </button>
         </div>
         <input
           type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           placeholder="Search mail"
           className="py-2 px-4 border border-gray-300 rounded-full w-64"
         />
@@ -124,9 +169,9 @@ const UserRegistry: React.FC = () => {
               <th className="text-left py-3 px-4 font-medium text-sm">
                 User Name
               </th>
-              <th className="text-left py-3 px-4 font-medium text-sm">
+              {/* <th className="text-left py-3 px-4 font-medium text-sm">
                 Account Type
-              </th>
+              </th> */}
               <th className="text-left py-3 px-4 font-medium text-sm">
                 Email Address
               </th>
@@ -137,86 +182,98 @@ const UserRegistry: React.FC = () => {
                 No. of Collaborators
               </th>
               <th className="text-left py-3 px-4 font-medium text-sm">Plan</th>
+              <th className="text-left py-3 px-4 font-medium text-sm">
+                Created Date
+              </th>
             </tr>
           </thead>
           <tbody>
-            {
-              isLoading ? (
-                <tr>
-                  <td colSpan={6} className="text-center ">
-                    <span className="flex justify-center items-center" >
+            {isLoading ? (
+              <tr>
+                <td colSpan={6} className="text-center ">
+                  <span className="flex justify-center items-center">
                     <FadeLoader height={10} radius={1} className="mt-3" />
-                    </span>
-                  </td>
-                </tr>
-              ) : error ? (
-                <tr>
-                  <td colSpan={6} className="text-center ">
-                    <span className="flex justify-center items-center text-xs text-red-500" >
-                    Something went wrong
-                    </span>
-                  </td>
-                </tr>
-              ) :
-            data?.data?.data?.map((user: any, index: number) => (
-              <tr
-                key={index}
-                className={`${
-                  index % 2 === 0 ? "bg-[#F7EEFED9]" : "bg-[#FEF5FED6]"
-                } text-sm rounded-md`}
-              >
-                <td className="py-3 px-4 flex items-center gap-2">
-                <Avatar className="size-8">
-                  <AvatarImage
-                    src={(user as any)?.photo_url ?? ""}
-                    alt="@johndoe"
-                  />
-                  <AvatarFallback className={`font-semibold  `} style={{backgroundColor: getRandomColor()}}>
-                    {generateInitials(user?.name ?? "")}
-                  </AvatarFallback>
-                </Avatar>
-                   {user?.name}</td>
-                <td className="py-3 px-4">
-                  {user?.accountType ? user?.accountType : "Not Available"}
-                </td>
-                <td className="py-3 px-4">{user?.email}</td>
-                <td
-                  className={`py-3 px-4 font-medium ${
-                    user.subscription === "Premium"
-                      ? "text-purple-600"
-                      : user.subscription === "Pro Plan"
-                      ? "text-red-600"
-                      : "text-green-600"
-                  }`}
-                >
-                  <span
-                    className={`py-1 px-2 rounded-full ${
-                      user.subscription === "Premium"
-                        ? "bg-[#D195FC1A]"
-                        : user.subscription === "Pro Plan"
-                        ? "bg-[#FFEBED]"
-                        : "bg-[#D3FAEC]"
-                    }`}
-                  >
-                    {user?.subscription ? user?.subscription : "Free Plan"}
                   </span>
                 </td>
-                <td className="py-3 px-4">
-                  {user?.collaborators ? user.collaborators : "Not Available"}
-                </td>
-                <td
-                  className={`py-3 px-4 font-medium ${
-                    user.plan === "Premium"
-                      ? "text-purple-600"
-                      : user.plan === "Pro Plan"
-                      ? "text-red-600"
-                      : "text-green-600"
-                  }`}
-                >
-                  {user?.plan ? user?.plan : "Free Plan"}
+              </tr>
+            ) : error ? (
+              <tr>
+                <td colSpan={6} className="text-center ">
+                  <span className="flex justify-center items-center text-xs text-red-500">
+                    Something went wrong
+                  </span>
                 </td>
               </tr>
-            ))}
+            ) : (
+              data?.data?.data?.map((user: any, index: number) => (
+                <tr
+                  key={index}
+                  className={`text-xs ${
+                    index % 2 === 0 ? "bg-[#F7EEFED9]" : "bg-[#FEF5FED6]"
+                  } text-sm rounded-md`}
+                >
+                  <td className="py-3 px-4 flex items-center gap-2">
+                    <Avatar className="size-8">
+                      <AvatarImage
+                        src={(user as any)?.photo_url ?? ""}
+                        alt="@johndoe"
+                      />
+                      <AvatarFallback
+                        className={`font-semibold  `}
+                        style={{ backgroundColor: getRandomColor() }}
+                      >
+                        {generateInitials(user?.name ?? "")}
+                      </AvatarFallback>
+                    </Avatar>
+                    {user?.name}
+                  </td>
+                  {/* <td className="py-3 px-4">
+                  {user?.accountType ? user?.accountType : "Not Available"}
+                </td> */}
+                  <td className="py-3 px-4">{user?.email}</td>
+                  <td
+                    className={`py-3 px-4 font-medium ${
+                      user.subscription === "Premium"
+                        ? "text-purple-600"
+                        : user.subscription === "Pro Plan"
+                        ? "text-red-600"
+                        : "text-green-600"
+                    }`}
+                  >
+                    <span
+                      className={`py-1 px-2 rounded-full ${
+                        user.subscription === "Premium"
+                          ? "bg-[#D195FC1A]"
+                          : user.subscription === "Pro Plan"
+                          ? "bg-[#FFEBED]"
+                          : "bg-[#D3FAEC]"
+                      }`}
+                    >
+                      {user?.subscription ? user?.subscription : "Free Plan"}
+                    </span>
+                  </td>
+                  <td className="py-3 text-center px-4">
+                    {user?.collaborators ? user.collaborators : "0"}
+                  </td>
+                  <td
+                    className={`py-3 px-4 font-medium ${
+                      user.plan === "Premium"
+                        ? "text-purple-600"
+                        : user.plan === "Pro Plan"
+                        ? "text-red-600"
+                        : "text-green-600"
+                    }`}
+                  >
+                    {user?.plan ? user?.plan : "Free Plan"}
+                  </td>
+                  <td className="py-3 px-4">
+                    {user?.createdAt
+                      ? formatDateOption(user.createdAt)
+                      : "Not Available"}
+                  </td>
+                </tr>
+              ))
+            )}
           </tbody>
         </table>
       </div>
