5328f46ad654b695f23bfba3744021ef6bfd7955
UbongJacob <ubongjacob13@gmail.com>
2025-01-23 20:05:12 +0100

    Added navigation to video tutorial list

--- src/components/resource-hub/VideoTutorial.tsx
+++ src/components/resource-hub/VideoTutorial.tsx
@@ -8,6 +8,8 @@ import { formatDate } from "@/lib/helpers";
 import { FadeLoader } from "react-spinners";
 import PageControl from "../common/PageControl";
 import { DEFAULT_API_PAGE_SIZE } from "@/services/api/tutorial";
+import Link from "next/link";
+import routes from "@/config/routes";
 
 const VideoTutorial = (): JSX.Element => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -59,37 +61,39 @@ const VideoTutorial = (): JSX.Element => {
               key={index}
               className="bg-white rounded-lg shadow-lg overflow-hidden border border-gray-200"
             >
-              <div
-                className={`relative min-h-48 flex justify-center items-center`}
-              >
-                {article.media[0].type === "image/jpeg" ||
-                article.media[0].type === "png" ||
-                article.media[0].type === "image/png" ? (
-                  <Image
-                    className="dark:invert w-full h-40 object-cover aspect-auto"
-                    src={article?.media[0]?.url}
-                    alt="Next.js logo"
-                    width={180}
-                    height={38}
-                    priority
-                  />
-                ) : (
-                  <video loop muted autoPlay className="w-full">
-                    <source src={article?.media[0]?.url} type="video/mp4" />
-                    Your browser does not support the video tag.
-                  </video>
-                )}
-              </div>
-              {/* <Image src={article.imageUrl} alt={article.title} className="w-full h-48 object-cover" /> */}
-              <div className="p-4">
-                <h2 className="text-lg font-semibold text-gray-900">
-                  {article.title}
-                </h2>
-                {/* <p className="text-gray-600 text-sm mt-2">{article.description}</p> */}
-                <p className="text-purple-600 font-semibold text-sm mt-4">
-                  {formatDate(article.createdAt)}
-                </p>
-              </div>
+              <Link href={routes.SINGLE_ARTICLE_PAGE(article?.slug)}>
+                <div
+                  className={`relative min-h-48 flex justify-center items-center`}
+                >
+                  {article.media[0].type === "image/jpeg" ||
+                  article.media[0].type === "png" ||
+                  article.media[0].type === "image/png" ? (
+                    <Image
+                      className="dark:invert w-full h-40 object-cover aspect-auto"
+                      src={article?.media[0]?.url}
+                      alt="Next.js logo"
+                      width={180}
+                      height={38}
+                      priority
+                    />
+                  ) : (
+                    <video loop muted autoPlay className="w-full">
+                      <source src={article?.media[0]?.url} type="video/mp4" />
+                      Your browser does not support the video tag.
+                    </video>
+                  )}
+                </div>
+                {/* <Image src={article.imageUrl} alt={article.title} className="w-full h-48 object-cover" /> */}
+                <div className="p-4">
+                  <h2 className="text-lg font-semibold text-gray-900">
+                    {article.title}
+                  </h2>
+                  {/* <p className="text-gray-600 text-sm mt-2">{article.description}</p> */}
+                  <p className="text-purple-600 font-semibold text-sm mt-4">
+                    {formatDate(article.createdAt)}
+                  </p>
+                </div>
+              </Link>
             </div>
           ))
         )}
