6424769dbe1e15d6ac7dcb233dd0f05d5b88dd7f
Jesse Abuaja <jesseabuaja@hotmail.com>
2025-03-18 12:02:20 +0100

    Task: staging-v2 config complete

--- src/services/config/base.ts
+++ src/services/config/base.ts
@@ -10,13 +10,13 @@ interface EnvironmentConfig {
 
 const environment: EnvironmentConfig = {
   production: {
-    API_BASE_URL: "https://pollsensei-api-a0e832048911.herokuapp.com/api/v1",
+    // API_BASE_URL: "https://pollsensei-api-a0e832048911.herokuapp.com/api/v1",
+    API_BASE_URL:
+      "https://pollsensei-staging-v2-3a23f320c170.herokuapp.com/api/v1",
   },
   development: {
     API_BASE_URL:
-      "https://pollsensei-api-dev-2e52be17da18.herokuapp.com/api/v1",
-
-    // API_BASE_URL: "https://pollsensei-api-a0e832048911.herokuapp.com/api/v1",
+      "https://pollsensei-staging-v2-3a23f320c170.herokuapp.com/api/v1",
   },
 };
 
