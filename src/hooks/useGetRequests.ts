a9e4a86f4ab15a81bd1b3b6c7c53ee0e9f66e462
Jesse Abuaja <jesseabuaja@hotmail.com>
2025-04-08 01:52:18 +0100

    fix: Table data missing for some chart components

--- src/hooks/useGetRequests.ts
+++ src/hooks/useGetRequests.ts
@@ -1,6 +1,6 @@
 import { handleApiErrors, isValidResponse } from "@/lib/utils";
 import { queryKeys, TUTORIAL_ENUM } from "@/services/api/constants.api";
-import { getTutorials } from "@/services/api/tutorial";
+import { getAdminTutorials } from "@/services/api/tutorial";
 import { useQuery } from "@tanstack/react-query";
 
 export interface UseGetTutorialsProps {
@@ -14,7 +14,7 @@ export const useGetTutorials = (props: UseGetTutorialsProps) => {
   return useQuery({
     queryKey: [queryKeys.TUTORIALS, filter, page],
     queryFn: async () => {
-      const response = await getTutorials(props);
+      const response = await getAdminTutorials(props);
       if (isValidResponse(response)) {
         return response?.data;
       } else {
