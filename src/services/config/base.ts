fa0cf80143ca396be62067ca013e630d32b3b267
Jesse Abuaja <jesseabuaja@hotmail.com>
2025-05-18 17:56:49 +0100

    base config updated for multiple environments

--- src/services/config/base.ts
+++ src/services/config/base.ts
@@ -6,24 +6,26 @@ interface Environment {
 interface EnvironmentConfig {
   production: Environment;
   development: Environment;
+  staging: Environment;
+  release: Environment;
 }
 
 const environment: EnvironmentConfig = {
+  // For main branch
   production: {
-    // API_BASE_URL: "https://pollsensei-api-dev-2e52be17da18.herokuapp.com/api/v1",
-    // API_BASE_URL: "https://ad5a-105-112-113-131.ngrok-free.app/api/v1",
-
-    API_BASE_URL: "https://pollsensei-api-a0e832048911.herokuapp.com/api/v1",
+    API_BASE_URL: "https://api.pollsensei.ai/api/v1",
   },
+  // For v2-main branch
   development: {
-    // API_BASE_URL: "https://pollsensei-api-1.onrender.com/api/v1",
-    API_BASE_URL:
-      // "https://ad5a-105-112-113-131.ngrok-free.app/api/v1",
-
-      // "https://6f55-203-159-81-29.ngrok-free.app/api/v1",
-      "https://pollsensei-api-dev-2e52be17da18.herokuapp.com/api/v1",
-
-    // API_BASE_URL: "https://pollsensei-api-a0e832048911.herokuapp.com/api/v1",
+    API_BASE_URL: "https://api-staging.pollsensei.ai/api/v1",
+  },
+  // For staging-v2 branch
+  staging: {
+    API_BASE_URL: "https://api-staging-v2.pollsensei.ai/api/v1",
+  },
+  // For release branch
+  release: {
+    API_BASE_URL: "https://api.pollsensei.ai/api/v1",
   },
 };
 
