f02d49350f754f5d0a25eb37278459f5991b52e2
Jesse Abuaja <jesseabuaja@hotmail.com>
2025-07-22 11:22:41 +0100

    Work done

--- src/redux/slices/questions.slice.ts
+++ src/redux/slices/questions.slice.ts
@@ -1,3 +1,87 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const initialState: [
+//   {
+//     questions: any;
+//     sectionTopic: string;
+//     sectionDescription: string;
+//   }
+// ] = [
+//   {
+//     questions: [],
+//     sectionTopic: "",
+//     sectionDescription: "",
+//   },
+// ];
+
+// const questionSlice = createSlice({
+//   name: "questions",
+//   initialState,
+//   reducers: {
+//     setQuestionObject: (_state, action) => {
+//       return [
+//         {
+//           questions: action.payload.questions || [],
+//           sectionTopic: action.payload.sectionTopic || "",
+//           sectionDescription: action.payload.sectionDescription || "",
+//         },
+//       ];
+//     },
+//     updateSectionDescription: (state, action) => {
+//       state[action.payload.index].sectionDescription = action.payload.data;
+//     },
+//     updateSectionTopic: (state, action) => {
+//       state[action.payload.index].sectionTopic = action.payload.data;
+//     },
+
+//     updateQuestions: (state, action) => {
+//       state[action.payload.index].questions = action.payload.data;
+//     },
+//     addQuestion: (state, action) => {
+//       state[action.payload.index].questions.push(action.payload.data as never);
+//     },
+//     deleteQuestion: (state, action) => {
+//       state[action.payload.index].questions.splice(
+//         action?.payload?.editIndex ?? action.payload.data,
+//         1
+//       );
+//     },
+//     updateQuestion: (
+//       state,
+//       action: {
+//         payload: { index: number; updatedQuestion: any; sectionIndex: number };
+//       }
+//     ) => {
+//       const { index, updatedQuestion } = action.payload;
+//       if (
+//         index >= 0 &&
+//         index < state[action.payload.sectionIndex].questions.length
+//       ) {
+//         state[action.payload.sectionIndex].questions[index] = updatedQuestion;
+//       } else {
+//         console.error("Invalid index for updating question");
+//       }
+//     },
+//     resetQuestion: (_state) => {
+//       return initialState;
+//     },
+//   },
+// });
+
+// export const {
+//   setQuestionObject,
+//   updateQuestions,
+//   addQuestion,
+//   deleteQuestion,
+//   updateSectionDescription,
+//   updateSectionTopic,
+//   resetQuestion,
+//   updateQuestion,
+//   setSurveyCreated,
+// } = questionSlice.actions;
+
+// export default questionSlice.reducer;
+
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState: [
@@ -77,7 +161,83 @@ export const {
   updateSectionTopic,
   resetQuestion,
   updateQuestion,
-  setSurveyCreated,
 } = questionSlice.actions;
 
 export default questionSlice.reducer;
+
+// import { createSlice } from '@reduxjs/toolkit';
+
+// interface Question {
+//   question: string;
+//   options?: string[];
+//   question_type: string;
+//   is_required: boolean;
+// }
+
+// const initialState: {
+//   questions: Question[];
+//   sectionTopic: string;
+//   sectionDescription: string;
+// } = {
+//   questions: [],
+//   sectionTopic: '',
+//   sectionDescription: '',
+// };
+
+// const questionSlice = createSlice({
+//   name: 'questions',
+//   initialState,
+//   reducers: {
+//     setQuestionObject: (_state, action) => {
+//       return {
+//         questions: action.payload.questions,
+//         sectionTopic: action.payload.sectionTopic,
+//         sectionDescription: action.payload.sectionDescription,
+//       };
+//     },
+//     updateSectionDescription: (state, action) => {
+//       state.sectionDescription = action.payload;
+//     },
+//     updateSectionTopic: (state, action) => {
+//       state.sectionTopic = action.payload;
+//     },
+//     updateQuestions: (state, action) => {
+//       state.questions = action.payload;
+//     },
+//     // addQuestion: (state, action) => {
+//     //   state.questions.push(action.payload);
+//     // },
+//       //   addQuestion: (state, action) => {
+//       // state.questions.push(action.payload as never);},
+//       addQuestion: (state, action) => {
+//         state.questions = [...state.questions, action.payload]; // Ensure immutability
+//       },
+//     deleteQuestion: (state, action) => {
+//       state.questions.splice(action.payload, 1);
+//     },
+//     updateQuestion: (state, action: { payload: { index: number; updatedQuestion: any } }) => {
+//       const { index, updatedQuestion } = action.payload;
+//       if (index >= 0 && index < state.questions.length) {
+//         state.questions[index] = updatedQuestion;
+//       } else {
+//         console.error('Invalid index for updating question');
+//       }
+//     },
+//     resetQuestion: () => {
+//       return initialState;
+//     },
+//   },
+// });
+
+// export const {
+//   setQuestionObject,
+//   updateQuestions,
+//   addQuestion,
+//   deleteQuestion,
+//   updateSectionDescription,
+//   updateSectionTopic,
+//   updateQuestion,
+//   resetQuestion,
+// } = questionSlice.actions;
+
+// export default questionSlice.reducer;
